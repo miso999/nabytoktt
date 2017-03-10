5a707d9b151aa329cfd4efb221f5d387a88eff34
miso999 <michal.simoncic@gmail.com>
2017-03-10 21:24:15 +0100

    10

--- base-front-page.php
+++ base-front-page.php
@@ -9,29 +9,14 @@ use Roots\Sage\Extras;
 <html <?php language_attributes(); ?>>
 <?php get_template_part('templates/head'); ?>
 <body <?php body_class(); ?>>
-<!--[if IE]>
-<div class="alert alert-warning">
-  <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage'); ?>
-</div>
-<![endif]-->
+
 <?php
 do_action('get_header');
 get_template_part('templates/header');
 ?>
 
 <header id="page-top">
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-12">
-        <div class="intro-text">
-          <span class="name">Nábytok na mieru</span>
-          <hr class="star-light">
-          <p class="skills">Landing Page Layout</p>
-          <p class="skills">Scroll to see the effect</p>
-        </div>
-      </div>
-    </div>
-  </div>
+    <img src="<?=Extras\ImagePath()?>/sldier.jpg">
 </header>
 <div class="content-wrapper">
   <?php
--- templates/portfolio.php
+++ templates/portfolio.php
@@ -1,6 +1,27 @@
 <?php
 use Roots\Sage\Extras;
+
+$query_images_args = array(
+    'post_type' => 'attachment',
+    'post_mime_type' =>'image',
+    'post_status' => 'inherit',
+    'posts_per_page' => -1,
+    'nabytok' => 'postele,kuchyne,satniky,schody'
+
+    );
+
+$query_images = new WP_Query( $query_images_args );
+$images = array();
+foreach ( $query_images->posts as $index=>$image) {
+
+    $terms =  wp_get_post_terms($image->ID, 'nabytok', array("fields" => "slugs"));
+    $images[$index]['category'] =  $terms[0];
+    $images[$index]['large'] = wp_get_attachment_image_src( $image->ID, 'large')[0];
+    $images[$index]['thumb'] = wp_get_attachment_image_src( $image->ID, 'thumbnail  ')[0];
+}
+
 ?>
+
 
 
 <section class="primary filter-section" id="portfolio">
@@ -22,11 +43,12 @@ use Roots\Sage\Extras;
 
                 <div class="filter-container isotopeFilters">
                     <ul class="list-inline filter">
-                        <li class="active list-inline-item"><a href="#" data-filter="*">Všetko </a><span>|</span></li>
-                        <li class="list-inline-item"><a href="#" data-filter=".kuchyne">Kuchyne</a><span>|</span></li>
+
+                        <li class="list-inline-item active"><a href="#" data-filter=".kuchyne">Kuchyne</a><span>|</span></li>
                         <li class="list-inline-item"><a href="#" data-filter=".schody">Schody</a><span>|</span></li>
                         <li class="list-inline-item"><a href="#" data-filter=".postele">Postele</a><span>|</span></li>
-                        <li class="list-inline-item"><a href="#" data-filter=".exterier">Exteriér</a></li>
+                        <li class="list-inline-item"><a href="#" data-filter=".exterier">Exteriér</a><span>|</span></li>
+                        <li class="list-inline-item"><a href="#" data-filter="*">Všetko </a></li>
                     </ul>
                 </div>
 
@@ -40,308 +62,17 @@ use Roots\Sage\Extras;
 
         <div class="isotopeContainer">
 
-            <div class="col-sm-4 isotopeSelector schody ">
+            <?php foreach ($images as $image) { ?>
+            <div class="col-sm-4 isotopeSelector <?=$image[category]?> ">
                 <article class="">
                     <figure>
-                        <a title="First Image" class="fancybox-pop" rel="portfolio-1" href="<?=Extras\ImagePath()?>portfolio/schody/IMG_3673.JPG">
-                            <img src="<?=Extras\ImagePath()?>portfolio/schody/IMG_3673.JPG" alt="">
+                        <a title="First Image" class="fancybox-pop" rel="portfolio-1" href="<?=$image[large]?>">
+                            <img src="<?=$image[thumb]?>" alt="">
                         </a>
                     </figure>
                 </article>
             </div>
-
-            <div class="col-sm-4 isotopeSelector schody ">
-                <article class="">
-                    <figure>
-                        <a title="First Image" class="fancybox-pop" rel="portfolio-1" href="<?=Extras\ImagePath()?>portfolio/schody/IMG_4107.JPG"><img src="<?=Extras\ImagePath()?>portfolio/schody/IMG_4107.JPG" alt=""></a>
-                    </figure>
-                </article>
-            </div>
-
-            <div class="col-sm-4 isotopeSelector schody ">
-                <article class="">
-                    <figure>
-                        <a title="First Image" class="fancybox-pop" rel="portfolio-1" href="<?=Extras\ImagePath()?>portfolio/schody/IMG_4199.JPG"><img src="<?=Extras\ImagePath()?>portfolio/schody/IMG_4199.JPG" alt=""></a>
-                    </figure>
-                </article>
-            </div>
-
-            <div class="col-sm-4 isotopeSelector schody ">
-                <article class="">
-                    <figure>
-
-                        <a title="First Image" class="fancybox-pop" rel="portfolio-1" href="<?=Extras\ImagePath()?>portfolio/schody/IMG_4140.JPG"><img src="<?=Extras\ImagePath()?>portfolio/schody/IMG_4140.JPG" alt=""></a>
-                    </figure>
-
-                </article>
-            </div>
-
-            <div class="col-sm-4 isotopeSelector schody ">
-                <article class="">
-                    <figure>
-
-
-
-
-
-                        <a title="First Image" class="fancybox-pop" rel="portfolio-1" href="<?=Extras\ImagePath()?>portfolio/schody/IMG_4243.JPG">                        <img src="<?=Extras\ImagePath()?>portfolio/schody/IMG_4243.JPG" alt="">
-                        </a>
-
-
-
-                    </figure>
-
-                </article>
-            </div>
-
-            <div class="col-sm-4 isotopeSelector schody ">
-                <article class="">
-                    <figure>
-
-
-
-
-
-
-                        <a title="Schody" class="fancybox-pop" rel="portfolio-1" href="<?=Extras\ImagePath()?>portfolio/schody/IMG_3686.JPG"><img src="<?=Extras\ImagePath()?>portfolio/schody/IMG_3686.JPG" alt=""></a>
-
-
-
-                    </figure>
-
-                </article>
-            </div>
-
-
-            <div class="col-sm-4 isotopeSelector schody ">
-                <article class="">
-                    <figure>
-
-
-
-
-
-
-                        <a title="First Image" class="fancybox-pop" rel="portfolio-1" href="<?=Extras\ImagePath()?>portfolio/schody/IMG_4184.JPG"><img src="<?=Extras\ImagePath()?>portfolio/schody/IMG_4184.JPG" alt=""></a>
-
-
-
-                    </figure>
-
-                </article>
-            </div>
-
-            <div class="col-sm-4 isotopeSelector schody ">
-                <article class="">
-                    <figure>
-                        <img src="<?=Extras\ImagePath()?>portfolio/schody/IMG_4133.JPG" alt="">
-
-
-
-
-
-                        <a title="First Image" class="fancybox-pop" rel="portfolio-1" href="<?=Extras\ImagePath()?>portfolio/schody/IMG_4133.JPG"></a>
-
-
-
-                    </figure>
-
-                </article>
-            </div>
-
-
-            <div class="col-sm-4 isotopeSelector kuchyne ">
-                <article class="">
-                    <figure>
-                        <img src="<?=Extras\ImagePath()?>portfolio/kuchyne/IMG_3670.JPG" alt="">
-
-
-
-
-
-                        <a title="First Image" class="fancybox-pop" rel="portfolio-1" href="<?=Extras\ImagePath()?>portfolio/kuchyne/IMG_3670.JPG"></a>
-
-                    </figure>
-
-                </article>
-            </div>
-
-            <div class="col-sm-4 isotopeSelector kuchyne ">
-                <article class="">
-                    <figure>
-                        <img src="<?=Extras\ImagePath()?>portfolio/kuchyne/IMG_4173.JPG" alt="">
-
-
-
-
-
-                        <a title="First Image" class="fancybox-pop" rel="portfolio-1" href="<?=Extras\ImagePath()?>portfolio/kuchyne/IMG_4173.JPG"></a>
-
-                    </figure>
-
-                </article>
-            </div>
-
-            <div class="col-sm-4 isotopeSelector kuchyne ">
-                <article class="">
-                    <figure>
-                        <img src="<?=Extras\ImagePath()?>portfolio/kuchyne/IMG_4201.JPG" alt="">
-
-
-
-
-
-                        <a title="First Image" class="fancybox-pop" rel="portfolio-1" href="<?=Extras\ImagePath()?>portfolio/kuchyne/IMG_4201.JPG"></a>
-
-                    </figure>
-
-                </article>
-            </div>
-
-            <div class="col-sm-4 isotopeSelector postele ">
-                <article class="">
-                    <figure>
-                        <img src="<?=Extras\ImagePath()?>portfolio/postele/IMG_9518.JPG" alt="">
-
-
-
-
-
-                        <a title="First Image" class="fancybox-pop" rel="portfolio-1" href="<?=Extras\ImagePath()?>portfolio/postele/IMG_9518.JPG"></a>
-
-                    </figure>
-
-                </article>
-            </div>
-
-
-            <div class="col-sm-4 isotopeSelector postele ">
-                <article class="">
-                    <figure>
-                        <img src="<?=Extras\ImagePath()?>portfolio/postele/IMG_9550.JPG" alt="">
-
-
-
-
-
-                        <a title="First Image" class="fancybox-pop" rel="portfolio-1" href="<?=Extras\ImagePath()?>portfolio/postele/IMG_9550.JPG"></a>
-
-                    </figure>
-
-                </article>
-            </div>
-
-            <div class="col-sm-4 isotopeSelector postele ">
-                <article class="">
-                    <figure>
-                        <img src="<?=Extras\ImagePath()?>portfolio/postele/IMG_9588.JPG" alt="">
-
-
-
-
-
-                        <a title="First Image" class="fancybox-pop" rel="portfolio-1" href="<?=Extras\ImagePath()?>portfolio/postele/IMG_9588.JPG"></a>
-
-                    </figure>
-
-                </article>
-            </div>
-
-            <div class="col-sm-4 isotopeSelector postele ">
-                <article class="">
-                    <figure>
-                        <img src="<?=Extras\ImagePath()?>portfolio/postele/IMG_9563.JPG" alt="">
-
-
-
-
-
-                        <a title="First Image" class="fancybox-pop" rel="portfolio-1" href="<?=Extras\ImagePath()?>portfolio/postele/IMG_9563.JPG"></a>
-
-                    </figure>
-
-                </article>
-            </div>
-
-            <div class="col-sm-4 isotopeSelector postele ">
-                <article class="">
-                    <figure>
-                        <img src="<?=Extras\ImagePath()?>portfolio/postele/IMG_4102.JPG" alt="">
-
-
-
-
-
-                        <a title="First Image" class="fancybox-pop" rel="portfolio-1" href="<?=Extras\ImagePath()?>portfolio/postele/IMG_4102.JPG"></a>
-
-                    </figure>
-
-                </article>
-            </div>
-
-            <div class="col-sm-4 isotopeSelector postele ">
-                <article class="">
-                    <figure>
-                        <img src="<?=Extras\ImagePath()?>portfolio/postele/IMG_4122.JPG" alt="">
-
-
-
-
-
-                        <a title="First Image" class="fancybox-pop" rel="portfolio-1" href="<?=Extras\ImagePath()?>portfolio/postele/IMG_4122.JPG"></a>
-
-                    </figure>
-
-                </article>
-            </div>
-
-            <div class="col-sm-4 isotopeSelector exterier ">
-                <article class="">
-                    <figure>
-                        <img src="<?=Extras\ImagePath()?>portfolio/exterier/IMG_7892.JPG" alt="">
-
-
-
-
-
-                        <a title="First Image" class="fancybox-pop" rel="portfolio-1" href="<?=Extras\ImagePath()?>portfolio/exterier/IMG_7892.JPG"></a>
-
-                    </figure>
-
-                </article>
-            </div>
-
-            <div class="col-sm-4 isotopeSelector exterier ">
-                <article class="">
-                    <figure>
-                        <img src="<?=Extras\ImagePath()?>portfolio/exterier/IMG_7896.JPG" alt="">
-
-
-
-
-
-                        <a title="First Image" class="fancybox-pop" rel="portfolio-1" href="<?=Extras\ImagePath()?>portfolio/exterier/IMG_7896.JPG"></a>
-
-                    </figure>
-
-                </article>
-            </div>
-
-            <div class="col-sm-4 isotopeSelector exterier ">
-                <article class="">
-                    <figure>
-                        <img src="<?=Extras\ImagePath()?>portfolio/exterier/IMG_7894.JPG" alt="">
-
-
-
-
-
-                        <a title="First Image" class="fancybox-pop" rel="portfolio-1" href="<?=Extras\ImagePath()?>portfolio/exterier/IMG_7894.JPG"></a>
-
-                    </figure>
-
-                </article>
-            </div>
+            <?php } ?>
 
         </div>
 
