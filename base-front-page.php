421ae17fde5042951ebfe70ab26a9ca987056ef6
miso999 <michal.simoncic@gmail.com>
2017-04-07 19:44:28 +0200

    Upraveny padding main, menu, pridany kontakt

--- base-front-page.php
+++ base-front-page.php
@@ -19,9 +19,24 @@ get_template_part('templates/header');
     <img src="<?=Extras\ImagePath()?>/sldier.jpg">
 </header>
 <div class="content-wrapper">
+
   <?php
   get_template_part('templates/portfolio');
   ?>
+
+<section class="section-kontakt" align="center">
+	<div style="text-align: center">
+        <div>
+            <span class="section-title"> Kontakt </span>
+        </div>
+        <span class="title-separator"></span>
+  
+    </div>
+
+  <?php
+   echo do_shortcode('[contact-form-7 id="44" title="Contact form 1"]');
+  ?>
+</section>
 
 
 
