4d8282b0da415a77ef5a54005860710d0cb2b0a9
miso999 <michal.simoncic@gmail.com>
2017-04-13 08:13:01 +0000

    odstranena mapa z footru

--- templates/footer.php
+++ templates/footer.php
@@ -5,7 +5,7 @@
              <?php dynamic_sidebar('sidebar-footer'); ?>
          </div>
          <div class="col-sm-6">
-             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3750.33587555614!2d17.565760754499696!3d48.34345941638474!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476ca0070c67e9d9%3A0xab81288952108dda!2sSv.+Martina+4779%2F64%2C+919+35+Hrn%C4%8Diarovce+nad+Parnou!5e0!3m2!1ssk!2ssk!4v1489851427671" width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
+             <!--iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3750.33587555614!2d17.565760754499696!3d48.34345941638474!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476ca0070c67e9d9%3A0xab81288952108dda!2sSv.+Martina+4779%2F64%2C+919+35+Hrn%C4%8Diarovce+nad+Parnou!5e0!3m2!1ssk!2ssk!4v1489851427671" width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe-->
          </div>
      </div>
  </div>
